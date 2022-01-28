2dcf099eb09954f868772128b26a957cef272b30
Henry Rodrigues <85399516+hhenry-rodrigues@users.noreply.github.com>
2022-01-27 21:50:06 -0300

    Add files via upload

---
+++ VGZsPN.js
@@ -0,0 +1,130 @@
+// codes.flcompy.com/api/cat/munchkin
+// Formless./Devs API for easy customization of select and option HTML and CSS.
+
+// API in the MIT license - https://pages.flcompy.com/license/mit/
+
+// Start API
+
+!(function (e) {
+  e.fn.flcompySelect = function (t) {
+    function s(t) {
+      t.after(
+        e("<div></div>")
+          .addClass("flcompy-select")
+          .addClass(t.attr("class") || "")
+          .addClass(t.attr("disabled") ? "disabled" : "")
+          .attr("tabindex", t.attr("disabled") ? null : "0")
+          .html('<span class="current"></span><ul class="list"></ul>')
+      );
+      var s = t.next(),
+        n = t.find("option"),
+        i = t.find("option:selected");
+      s.find(".current").html(i.data("display") || i.text()),
+        n.each(function (t) {
+          var n = e(this),
+            i = n.data("display");
+          s.find("ul").append(
+            e("<li></li>")
+              .attr("data-value", n.val())
+              .attr("data-display", i || null)
+              .addClass(
+                "option" +
+                  (n.is(":selected") ? " selected" : "") +
+                  (n.is(":disabled") ? " disabled" : "")
+              )
+              .html(n.text())
+          );
+        });
+    }
+    if ("string" == typeof t)
+      return (
+        "update" == t
+          ? this.each(function () {
+              var t = e(this),
+                n = e(this).next(".flcompy-select"),
+                i = n.hasClass("open");
+              n.length && (n.remove(), s(t), i && t.next().trigger("click"));
+            })
+          : "destroy" == t
+          ? (this.each(function () {
+              var t = e(this),
+                s = e(this).next(".flcompy-select");
+              s.length && (s.remove(), t.css("display", ""));
+            }),
+            0 == e(".flcompy-select").length &&
+              e(document).off(".flcompy_select"))
+          : console.log('Method "' + t + '" does not exist.'),
+        this
+      );
+    this.hide(),
+      this.each(function () {
+        var t = e(this);
+        t.next().hasClass("flcompy-select") || s(t);
+      }),
+      e(document).off(".flcompy_select"),
+      e(document).on("click.flcompy_select", ".flcompy-select", function (t) {
+        var s = e(this);
+        e(".flcompy-select").not(s).removeClass("open"),
+          s.toggleClass("open"),
+          s.hasClass("open")
+            ? (s.find(".option"),
+              s.find(".focus").removeClass("focus"),
+              s.find(".selected").addClass("focus"))
+            : s.focus();
+      }),
+      e(document).on("click.flcompy_select", function (t) {
+        0 === e(t.target).closest(".flcompy-select").length &&
+          e(".flcompy-select").removeClass("open").find(".option");
+      }),
+      e(document).on(
+        "click.flcompy_select",
+        ".flcompy-select .option:not(.disabled)",
+        function (t) {
+          var s = e(this),
+            n = s.closest(".flcompy-select");
+          n.find(".selected").removeClass("selected"), s.addClass("selected");
+          var i = s.data("display") || s.text();
+          n.find(".current").text(i),
+            n.prev("select").val(s.data("value")).trigger("change");
+        }
+      ),
+      e(document).on("keydown.flcompy_select", ".flcompy-select", function (t) {
+        var s = e(this),
+          n = e(s.find(".focus") || s.find(".list .option.selected"));
+        if (32 == t.keyCode || 13 == t.keyCode)
+          return (
+            s.hasClass("open") ? n.trigger("click") : s.trigger("click"), !1
+          );
+        if (40 == t.keyCode) {
+          if (s.hasClass("open")) {
+            var i = n.nextAll(".option:not(.disabled)").first();
+            i.length > 0 &&
+              (s.find(".focus").removeClass("focus"), i.addClass("focus"));
+          } else s.trigger("click");
+          return !1;
+        }
+        if (38 == t.keyCode) {
+          if (s.hasClass("open")) {
+            var l = n.prevAll(".option:not(.disabled)").first();
+            l.length > 0 &&
+              (s.find(".focus").removeClass("focus"), l.addClass("focus"));
+          } else s.trigger("click");
+          return !1;
+        }
+        if (27 == t.keyCode) s.hasClass("open") && s.trigger("click");
+        else if (9 == t.keyCode && s.hasClass("open")) return !1;
+      });
+    var n = document.createElement("a").style;
+    return (
+      (n.cssText = "pointer-events:auto"),
+      "auto" !== n.pointerEvents && e("html").addClass("no-csspointerevents"),
+      this
+    );
+  };
+})(jQuery);
+
+$(document).ready(function () {
+  $(".my-munchkin").flcompySelect();
+});
+
+// End API
